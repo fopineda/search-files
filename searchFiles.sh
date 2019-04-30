904ef727f55ed9dad4ce48c8ab99a67bf4ead43b
Felipe Pineda <fpineda@live.unc.edu>
2019-04-29 22:58:01 -0400

    Added output to txt file
    After you run the script it should output to a txt file called found-paths.txt

--- searchFiles.sh
+++ searchFiles.sh
@@ -8,8 +8,8 @@
 # Path to file with with search word found in
 
 
-## Run: bash searchFiles.sh "/Users/pinmac/Documents/Bash-Scripts/test" "ecourse"
+## Run: bash searchFiles.sh "/Users/pinmac/Documents/Github/search-files/test" "ecourse"
 ## May need to chmod for permissions
 ## Don't remember if you need to compile this, but I was able to run it without
 
-find $1 -type f -print0 | xargs -0 grep -l $2
+find $1 -type f -print0 | xargs -0 grep -l $2 > found-paths.txt
